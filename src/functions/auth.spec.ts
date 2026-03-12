af296ed281329ee60ac609ef480e9426098aabc0
William Durand <will+git@drnd.me>
2026-03-12 14:47:58 +0100

    Add a new downloadFile() function (#1007)

--- src/functions/auth.spec.ts
+++ src/functions/auth.spec.ts
@@ -7,27 +7,21 @@ describe(__filename, () => {
     const issKey = 'test-iss-key';
     const secret = 'test-secret';
 
-    const makeProcess = (env: Record<string, string> = {}) =>
-      ({ env }) as unknown as typeof process;
-
     it('throws when AMO_JWT_ISS_KEY is not set', () => {
-      expect(() =>
-        makeJWT({ _process: makeProcess({ AMO_JWT_SECRET: secret }) }),
-      ).toThrow('AMO_JWT_ISS_KEY environment variable is not set');
+      expect(() => makeJWT({ env: { AMO_JWT_SECRET: secret } })).toThrow(
+        'AMO_JWT_ISS_KEY environment variable is not set',
+      );
     });
 
     it('throws when AMO_JWT_SECRET is not set', () => {
-      expect(() =>
-        makeJWT({ _process: makeProcess({ AMO_JWT_ISS_KEY: issKey }) }),
-      ).toThrow('AMO_JWT_SECRET environment variable is not set');
+      expect(() => makeJWT({ env: { AMO_JWT_ISS_KEY: issKey } })).toThrow(
+        'AMO_JWT_SECRET environment variable is not set',
+      );
     });
 
     it('returns a valid signed JWT', () => {
       const token = makeJWT({
-        _process: makeProcess({
-          AMO_JWT_ISS_KEY: issKey,
-          AMO_JWT_SECRET: secret,
-        }),
+        env: { AMO_JWT_ISS_KEY: issKey, AMO_JWT_SECRET: secret },
       });
 
       const decoded = jwt.verify(token, secret) as jwt.JwtPayload;
